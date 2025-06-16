2597f61dacc7ac281144e8f77528721a25d4075d
sungje-park <48614151+sungje-park@users.noreply.github.com>
2025-06-16 13:27:39 -0700

    implemented new container class

--- src/containers.py
+++ src/containers.py
@@ -12,53 +12,127 @@ sys.path.append(".")
 from src.dynamics import Dynamics
 from utils.utils import *
 from src.elements import *
+import jax.tree_util
+import numpy as np
 
-class Container:
-    def __init__(self,name:str):
-        self.name = name
-    def flatten(self):
-        pass
+@jax.tree_util.register_pytree_node_class
+class Container():
+    dynamics: Dynamics
+    '''
+    Generic class for all Containers, such as Cells, Nodes, Faces, etc.
+    '''
+    def __init__(self,size,dynamics: Dynamics):
+        '''
+        Initializes the Container with a given size and dynamics
+        '''
+        self.pdf = jnp.ones((size, dynamics.NUM_QUIVERS), dtype=jnp.float32)
+        self.dynamics = dynamics
 
-class ElementContainer(Container):
-    def __init__(self,name:str, elements: list[Element]):
-        super().__init__(name)
-        self.elements = elements
-
-    def flatten(self):
-        assert all(self.elements[0].__class__ == element.__class__ for element in self.elements), "ElementContainer: Element types differ"
-        assert len(self.elements) > 0, "ElementContainer: Empty List"
-        
-        dynamic,static = zip(*[element.flatten() for element in self.elements])
-
-        return pad_stack_tree(dynamic),pad_stack_tree(static)
-    
-class MultiElementContainer(Container):
-    def __init__(self,name:str,*args:Container):
-        super().__init__(name)
-        self.containers = [arg for arg in args]
-
+    # JAX flattening and unflattening functions for Container
+    def tree_flatten(self):
+        children = (self.pdf,)
+        aux_data = {'dynamics': self.dynamics}
+        return children, aux_data
     @classmethod
-    def create(cls,name:str,*args:Container):
-
-        temp = cls.__init__(name,[arg for arg in args])
-        return temp
-
-    def flatten(self):
-        dynamic = {}
-        static = {}
-        for container in self.containers:
-            dynamic[container.name],static[container.name] = container.flatten()
-        return dynamic,static
+    def tree_unflatten(cls, aux_data, children):
+        obj = cls.__new__(cls)
+        obj.pdf = children[0]
+        obj.dynamics = aux_data['dynamics']
+        return obj
     
-class NodeContainer(ElementContainer):
-    # Class Var
-    type_key = {0: "Internal",
-                1: "Velocity",
-                2: "Pressure"}
-    def __init__(self,name:str,elements: list[Element]):
-        super().__init__(name,elements)
+    def __repr__(self):
+        return repr(self.__dict__)
     
-    def flatten(self):
-        dynamic, static = super().flatten()
-        static, dynamic = splice_split_2dict(static,dynamic,'type',self.type_key)
-        return dynamic,static
+@jax.tree_util.register_pytree_node_class
+class Cells(Container):
+    '''
+    Class for Cells in the FVDBM solver.
+    Inherits from Container and initializes with the dynamics.
+    '''
+    def __init__(self, size, dynamics: Dynamics):
+        super().__init__(size, dynamics)
+        # Density and Velocity
+        self.rho = jnp.zeros((size, 1), dtype=jnp.float32)
+        self.vel = jnp.zeros((size, dynamics.DIM), dtype=jnp.float32)
+
+        self.face_indices = CustomArray(size, dtype=jnp.int32, default_value=-1)
+        self.face_normals = CustomArray(size, dtype=jnp.bool, default_value=0)
+        '''Face Normals (0 = negative, 1 = positive)'''
+
+    def init(self):
+        '''
+        Initializes Cell container for the FVDBM solver.
+        '''
+        self.face_indices = jnp.asarray(self.face_indices)
+        self.face_normals = jnp.asarray(self.face_normals)
+
+    # Jax flattening and unflattening functions for Cells
+    def tree_flatten(self):
+        children, aux_data = super().tree_flatten()
+
+        children += (self.rho, self.vel)
+        
+        aux_data["face_indices"] = self.face_indices
+        aux_data["face_normals"] = self.face_normals
+        return children, aux_data
+    @classmethod
+    def tree_unflatten(cls, aux_data, children):
+        obj = super().tree_unflatten({"dynamics":aux_data["dynamics"]}, children[:1])
+
+        obj.rho = children[1]
+        obj.vel = children[2]
+        obj.face_indices = aux_data["face_indices"]
+        obj.face_normals = aux_data["face_normals"]
+
+        return obj
+    
+    def calc_macros(self):
+        self.rho,self.vel = jax.jit(jax.vmap(self.dynamics.calc_macro))(self.pdf)
+
+class Faces(Container):
+    '''
+    Class for Faces in the FVDBM solver.
+    Inherits from Container and initializes with the dynamics.
+    '''
+    def __init__(self, size, dynamics: Dynamics):
+        super().__init__(size, dynamics)
+        self.flux = jnp.zeros((size, dynamics.NUM_QUIVERS), dtype=jnp.float32)
+        
+        #Static
+        self.nodes_index = CustomArray(size, dtype=jnp.int32, default_value=-1)
+        self.stencil_cells_index = CustomArray(size, dtype=jnp.int32, default_value=-1)
+        self.stencil_dists = CustomArray(size, dtype=jnp.float32, default_value=-1)
+        self.n = jnp.zeros((size, dynamics.DIM), dtype=jnp.float32)
+        self.L = jnp.zeros((size, 1), dtype=jnp.float32)
+
+    def init(self):
+        '''
+        Initializes Face container for the FVDBM solver.
+        '''
+        self.nodes_index = jnp.asarray(self.nodes_index)
+        self.stencil_cells_index = jnp.asarray(self.stencil_cells_index)
+        self.stencil_dists = jnp.asarray(self.stencil_dists)
+
+class Nodes(Container):
+    '''
+    Class for Nodes in the FVDBM solver.
+    Inherits from Container and initializes with the dynamics.
+    '''
+    def __init__(self, size, dynamics: Dynamics):
+        super().__init__(size, dynamics)
+        # Density and Velocity
+        self.rho = jnp.zeros((size, 1), dtype=jnp.float32)
+        self.vel = jnp.zeros((size, dynamics.DIM), dtype=jnp.float32)
+
+        self.type = jnp.zeros((size, 1), dtype=jnp.int32)  # Node type (e.g., boundary, internal)
+        self.cells_index = CustomArray(size, dtype=jnp.int32, default_value=-1)
+        self.cell_dists = CustomArray(size, dtype=jnp.float32, default_value=-1)
+
+    def init(self):
+        '''
+        Initializes Node container for the FVDBM solver.
+        '''
+        self.cells_index = jnp.asarray(self.cells_index)
+        self.cell_dists = jnp.asarray(self.cell_dists)
+
+    
